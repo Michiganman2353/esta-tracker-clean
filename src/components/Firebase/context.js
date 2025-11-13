09e9bdb231f6e0ab7cd853cc29902ae28845ae7c
Michiganman2353 <Hankins.dan@sjncanton.org>
2025-11-13 11:37:54 -0500

    Update context.js

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -1,11 +1,24 @@
+// components/firebase/context.js
 import React from 'react';
 
 const FirebaseContext = React.createContext(null);
 
-export const withFirebase = Component => props => (
-  <FirebaseContext.Consumer>
-    {firebase => <Component {...props} firebase={firebase} />}
-  </FirebaseContext.Consumer>
-);
+// Hook for components
+export const useFirebase = () => {
+  const context = React.useContext(FirebaseContext);
+  if (!context) {
+    throw new Error('useFirebase must be used within FirebaseProvider');
+  }
+  return context;
+};
+
+// Provider remains in App or Session
+export const FirebaseProvider = FirebaseContext.Provider;
+
+// Legacy HOC (keep for old components – remove later)
+export const withFirebase = (Component) => (props) => {
+  const firebase = useFirebase();
+  return <Component {...props} firebase={firebase} />;
+};
 
 export default FirebaseContext;
