b18bbe4ddeb885a32405fa48f1f5305193519be2
Michiganman2353 <Hankins.dan@sjncanton.org>
2025-11-13 11:52:35 -0500

    Update index.js

--- src/components/Messages/index.js
+++ src/components/Messages/index.js
@@ -1,3 +1,11 @@
+// src/components/messages/index.js
 import Messages from './Messages';
+import MessageList from './MessageList';
+import MessageItem from './MessageItem';
+import { useMessages } from './useMessages';
 
+// Default export
 export default Messages;
+
+// Named exports
+export { MessageList, MessageItem, useMessages };
